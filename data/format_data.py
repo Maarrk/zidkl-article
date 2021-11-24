1d3fe37fb79277649ec46235ae535d0490deb0b4
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2021-11-24 15:49:09 +0100

    Adjust figures for the article

--- data/format_data.py
+++ data/format_data.py
@@ -13,7 +13,7 @@ with open('example_2MG.txt', 'r') as file_in:
             time_in = int(words[0])
             if time_in != previous_second:
                 second_repetitions = 0
-            
+
             time_out = float(time_in) + second_repetitions * line_interval
             file_out.write(f'{time_out:.1f};{words[1]};{words[3]}\n')
 
